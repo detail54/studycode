3106565b468dae91f3d3c620b3c3a42818f33c0e
detail54 <73803940+detail54@users.noreply.github.com>
2021-01-20 22:15:24 +0900

    Add files via upload

---
+++ interfaceeeeeee.java
@@ -0,0 +1,29 @@
+package interfaceee;
+
+interface I1{
+	public void x();
+}
+		//접근 제어자는 반드시 public 이어야 한다.
+interface I2{
+	public void z();
+}
+		//implements - 클래스와 인터페이스 결합. (클래스 A는 인터페이스 I를 구현한다.)
+class A implements I1, I2{
+	public void x() {}
+	public void z() {}
+}
+		//클래스가 상위 클래스를 상속할땐 한개의 상위클래스를 상속 할수 있지만,
+		//인터페이스는 여러개 결합할 수 있다. 단, 결합한 인터페이스의 메소드를 반드시 구현을 해야함.
+
+interface I3{
+	public void a();
+}
+
+interface I4 extends I3{		//인터페이스도 상속가능. 상속은 인터페이스도 한개의 인터페이스만 상속가능.
+	public void b();			//단, 상속받은 하위 인터페이스를 클래스에 결합 시키면 상위 인터페이스와 하위 인터페이스의 메소드를 모두 구현해야함.
+}
+
+class B implements I4{
+	public void a() {}
+	public void b() {}
+}
